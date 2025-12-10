94ae0d5866485c1e3b62fe2efe8f9ebb5742d912
Nicolaas Nel <nicmeister@live.co.za>
2025-12-11 00:12:28 +0200

    fix: Local env changes broke EB env.

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,6 +1,10 @@
 # Configuration for Halo 5 API
-from dotenv import load_dotenv
-load_dotenv()
 import os
+if not os.environ.get("AWS_EXECUTION_ENV"):
+	try:
+		from dotenv import load_dotenv
+		load_dotenv()
+	except ImportError:
+		pass
 HALO_API_KEY = os.getenv("HALO_API_KEY", "")
 HALO_API_BASE_URL = "https://www.haloapi.com"
